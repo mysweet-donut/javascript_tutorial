ede63cec7e6bb77a5a22d189791f190a1f3849b6
iamdonut <lauren07070@naver.com>
2023-02-19 11:02:13 +0900

    accordion

---
+++ javascript_tutorial/05.accordian/app.js
@@ -0,0 +1,14 @@
+const content = document.querySelectorAll('.content')
+
+console.log(content)
+
+for(let i = 0; i < content.length; i++){
+    // content[i].addEventListener('click', () => {
+    //     console.log(this)
+    // })
+
+    content[i].addEventListener('click', function() {
+        //console.log(this)
+        this.classList.toggle('active')
+    })
+}
