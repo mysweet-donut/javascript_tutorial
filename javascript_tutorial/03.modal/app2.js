790a2ae011b2a05e376a2c7435c4c85b84397c4b
iamdonut <lauren07070@naver.com>
2023-02-19 09:48:47 +0900

    modal

---
+++ javascript_tutorial/03.modal/app2.js
@@ -0,0 +1,10 @@
+const content = document.querySelector('.content')
+const modal = document.querySelector('.modal')
+const btns = document.querySelectorAll('.btn')
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        content.classList.toggle('active')
+        modal.classList.toggle('active')
+    })
+})
